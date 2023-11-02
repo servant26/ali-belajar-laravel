c90e49f31f793186b4a09c93b20109c70cf66908
servant26 <alirpl16@gmail.com>
2023-11-03 03:21:08 +0800

    mengubah link gambar jadi foto diri sendiri

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Belajar Laravel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
@@ -37,7 +37,8 @@
               <tr>
                 <th rowspan="9" scope="">
                     <div class="text-center">
-                        <img src="https://i.pinimg.com/originals/88/51/4c/88514c8d853072a522281f716c0d64e2.jpg" height="200px" class="rounded" alt="...">
+                      <img src="https://drive.google.com/uc?id=15b6a-wOR0pG_yWYsxJWN3YI6Hyt-QyGZ" height="350px" class="rounded" alt="...">
+
                     </div>
                 </th>
               </tr>
