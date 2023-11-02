9065e159c02eb136140535c80448f99f9891a96d
servant26 <alirpl16@gmail.com>
2023-11-03 01:15:02 +0800

    membuat routing dan controller helloworld yang didalamnya terdapat view hello, didalam view hello sendiri berisi halaman biodata

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+<!--Awal Navbar-->
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div class="container-fluid m-2">
+      <a class="navbar-brand" href="#">Hello World</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item mx-3">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--Akhir navbar-->
+
+  <!--Awal Table-->
+  <div class="container-sm mt-5">
+    <div class="shadow p-3 mb-5 bg-body-tertiary rounded">
+
+        <div class="table-responsive">
+        <table class="table" table border="0">
+            <tbody>
+              <tr>
+                <th rowspan="9" scope="">
+                    <div class="text-center">
+                        <img src="https://i.pinimg.com/originals/88/51/4c/88514c8d853072a522281f716c0d64e2.jpg" height="200px" class="rounded" alt="...">
+                    </div>
+                </th>
+              </tr>
+              <tr>
+                <th>Nama</th>
+                <td>Ali Khatami</td>
+              </tr>
+              <tr>
+                <th>Motto hidup</th>
+                <td>Jalani, pelajari, manfaatkan dan terapkan</td>
+              </tr>
+              <tr>
+                <th scope="row">Asal Kampus</th>
+                <td>Universitas Mulawarman</td>
+              </tr>
+              <tr>
+                <th scope="row">Kelas</th>
+                <td>Fullstack Web Developer</td>
+              </tr>
+              <tr>
+                <th scope="row">Hobi</th>
+                <td>Programming, Sepakbola</td>
+              </tr>
+              <tr>
+                <th scope="row">Skill Programming yang dikuasai</th>
+                <td>HTML, CSS, Bootstrap, PHP, MySql, Laravel</td>
+              </tr>
+              <tr>
+                <th scope="row">Cita-cita</th>
+                <td>Programmer</td>
+              </tr>
+              <tr>
+                <th scope="row">Sosial Media</th>
+                <td>
+                    <a class="btn btn-dark" href="https://github.com/servant26"target="_blank" role="button">Github</a>
+                    <a class="btn btn-dark" href="tiktok.com/@servant_arthur07"target="_blank" role="button">Tiktok</a>
+                    <a class="btn btn-primary" href="https://www.facebook.com/otaku.art.35" target="_blank" role="button">Facebook</a>
+                    <a class="btn btn-danger" href="https://www.instagram.com/servant_arthur07/"target="_blank" role="button">Instagram</a>
+                    <a class="btn btn-danger" href="https://www.youtube.com/@alikhatami3910"target="_blank" role="button">Youtube</a>
+                </td>
+              </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
+    <a class="btn btn-primary mb-5" href="../" role="button">Kembali</a>
+  </div>
+  <!--Akhir Table-->
+</body>
+</html>
