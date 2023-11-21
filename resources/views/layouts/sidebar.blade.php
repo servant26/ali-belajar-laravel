231e8ce8e4bd2bd4124285c0e385684bc04fda40
servant26 <alirpl16@gmail.com>
2023-11-21 18:49:51 +0800

    memperbaiki sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -43,7 +43,8 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); 
+            document.getElementById('logout-form').submit();">
                 <i class="nav-icon fa fa-power-off"></i>
                 <p>Logout</p>
             </a>
